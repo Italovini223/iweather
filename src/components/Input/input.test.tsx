8a8b0d549435e3755cef2c4476a30c2c03c4a24b
Italovini223 <italovinicius2018@gmail.com>
2024-04-25 01:31:08 -0300

    ✅ add input component test

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react-native'
+import { Input } from '@components/Input'
+
+describe("Component: Input", () => {
+  it("should render the input component", () => {
+    render(<Input />)
+  })
+})  
